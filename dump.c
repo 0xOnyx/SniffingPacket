39a650b0dce1ea3c37383dea34e449f92782dbed
jeje720 <jeremy.dosg@gmail.com>
2021-07-03 17:15:00 +0200

    version

---
+++ dump.c
@@ -0,0 +1,49 @@
+
+#include "dump.h"
+
+
+void dump(const unsigned char *buff, unsigned int lenght)
+{
+    unsigned char bytes;
+
+    unsigned int i;
+    i = 0;
+printf("dump\n");
+    while(i < lenght)
+    {
+        bytes = buff[i];
+        printf("%02x ", bytes);
+        if( (i%16) == 15  ||  i == (lenght-1) )
+        {
+            unsigned int j;
+            j = 0;
+
+            while(j < 15-(i%16))
+            {
+                printf("   ");
+                j++;
+            }
+            putchar('|');
+
+            j = i-(i%16);
+
+            while(j < i)
+            {
+                bytes = buff[j];
+
+                if( (bytes > 31) && (bytes < 127) )
+                    printf("%c", bytes);
+                else
+                    putchar('.');
+
+                j++;    
+            }
+
+
+            putchar('\n');
+        } 
+
+        i += 1;
+    }
+
+}
---
+++ dump.h
@@ -0,0 +1,10 @@
+
+
+#ifndef DUMP_H
+#   define DUMP_H
+
+#   include <stdio.h>
+    void dump(const unsigned char *buff, unsigned int lenght);
+
+#endif
+
